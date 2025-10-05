cd07d88492c476f9efad6340ebcd0b0d025ea1cb
snehalini1234 <snehlini.duttamagni@gmail.com>
2025-10-05 12:31:02 +0530

    intialized

---
+++ main/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable static export output
+  output: 'export',
+  // You can add other production optimizations here
+}
+
+module.exports = nextConfig
